b05357302d3b95d93d69486fd174adb0ade5c73c
aliturcan34 <alikaganturcan@gmail.com>
2022-10-03 01:56:02 -0400

    updated index route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("welcome to the home page and changed content");
+  res.send("updating app");
 });
 
 const port = process.env.port || 3000;
